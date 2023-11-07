63f4f7d02df0e0aa7ffbfc6ec0f8abda799c75f2
lorena-21 <110685156+lorena-21@users.noreply.github.com>
2023-11-07 16:25:34 -0500

    clase

---
+++ src/Silla.java
@@ -0,0 +1,38 @@
+public class Silla {
+  //atributos
+    String color;
+    String forma;
+    String  dimension;
+    public static void main (String[] args){
+        Silla Silla1= new Silla();
+        Silla1.color = "gris";
+        Silla1.forma = "giratoria";
+        Silla1.dimension = "45-50 cm";
+       System.out.println("el color de la silla1 es: "+Silla1.color);
+       System.out.println("la forma de la silla1 es: "+Silla1.forma);
+        System.out.println("la dimension de su Silla1 es:"+Silla1.dimension);
+        Silla Silla2= new Silla();
+              Silla2.color = "negra";
+              Silla2.forma = "comedor";
+              Silla2 .dimension = "45-60 cm";
+              System.out.println("el color de la silla1 es: "+Silla2.color);
+               System.out.println("la forma de la silla1 es: "+Silla2.forma);
+               System.out.println("la dimension de su Silla1 es:"+Silla2.dimension);
+
+               Silla Silla3= new Silla();
+                     Silla3.color = "cafe";
+                     Silla3.forma = "cuadrada";
+                     Silla3 .dimension = "70-80 cm";
+                     System.out.println("el color de la silla1 es: "+Silla1.color);
+                      System.out.println("la forma de la silla1 es: "+Silla1.forma);
+                      System.out.println("la dimension de su Silla3 es:"+Silla3.dimension);
+
+
+        }
+
+    }
+
+
+
+
+
